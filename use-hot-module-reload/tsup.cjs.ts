9c046b71953c58fdc5c0656177966c8cda0ca890
Cody Olsen <stipsan@gmail.com>
2024-03-21 16:48:30 +0100

    fix: update ESM output to increase interop

--- use-hot-module-reload/tsup.cjs.ts
+++ use-hot-module-reload/tsup.cjs.ts
@@ -1,7 +1,7 @@
 import {defineConfig} from 'tsup'
 
 export default defineConfig({
-  entry: ['src/use-hot-module-reload.cjs.ts'],
+  entry: ['src/use-hot-module-reload.cts'],
   splitting: false,
   sourcemap: true,
   clean: false,
